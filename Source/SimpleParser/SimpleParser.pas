953f02c3d4afa21702679e0fe2139ab4bbe464d3
Irame <irame94@gmail.com>
2021-10-27 14:42:22 +0200

    Workaround for GreaterEqual symbol in TypeSimple

--- Source/SimpleParser/SimpleParser.pas
+++ Source/SimpleParser/SimpleParser.pas
@@ -5019,7 +5019,11 @@ begin
   begin
     Expected(ptLower);
     TypeArgs;
-    Expected(ptGreater);
+    // workaround for VAR List: TList<Integer>= nil;
+    if TokenID = ptGreaterEqual then
+      Lexer.RunPos := Lexer.RunPos - 1
+    else
+      Expected(ptGreater);
   end;
 end;
 
